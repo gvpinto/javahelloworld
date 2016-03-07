d153d2369093583cae6a29428efdad13d2695f93
Glenn Pinto <gvpinto@gmail.com>
2016-03-06 19:28:56 -0500

    New Sysout

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,6 +3,8 @@ public class JavaHelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Java Hello World");
 		System.out.println("Hello again");
+		System.out.println("Hello. Hopefully this is last ");
+
 	}
 
 }
